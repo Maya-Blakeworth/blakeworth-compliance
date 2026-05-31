2dbd9573e055062043c8bb48a9cd2d8f3a5ea220
Maya-Blakeworth <maya@blakeworth.com>
2026-05-31 12:45:18 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 // then re-publish and update these URLs.
 // ============================================================
 
-const CONFIG = {
+window.CONFIG = {
   MASTER_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFKs41099EB7y7pEFBuogrAlB33f7xxJ_cCcvKClW4P1e-hSzeWIDPKp8C4b_Ko2Xf-nXWgasKPrBP/pub?gid=1530981695&single=true&output=csv",
   DOCS_CSV_URL:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFKs41099EB7y7pEFBuogrAlB33f7xxJ_cCcvKClW4P1e-hSzeWIDPKp8C4b_Ko2Xf-nXWgasKPrBP/pub?gid=1675867444&single=true&output=csv",
   LINKS_CSV_URL:  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFKs41099EB7y7pEFBuogrAlB33f7xxJ_cCcvKClW4P1e-hSzeWIDPKp8C4b_Ko2Xf-nXWgasKPrBP/pub?gid=1821796381&single=true&output=csv",
