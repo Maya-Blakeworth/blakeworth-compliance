509104d4407a2ed32e80c0621054b9c1e7048914
Maya-Blakeworth <maya@blakeworth.com>
2026-07-20 05:40:06 -0400

    Update CSV URLs to point to Google Sheets

--- config.js
+++ config.js
@@ -7,9 +7,9 @@
 // ============================================================
 
 window.CONFIG = {
-  MASTER_CSV_URL: "data/master.csv",
-  DOCS_CSV_URL:   "data/docs.csv",
-  LINKS_CSV_URL:  "data/links.csv",
+  MASTER_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNae-sHgBR0y3X1VUQlOwl-gQsR8D9GqB6SS79Nd_5eFqpXp4V9G5GD-Zmhth22TBfsnZU5nUVGFyf/pub?gid=1135568484&single=true&output=csv",
+  DOCS_CSV_URL:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNae-sHgBR0y3X1VUQlOwl-gQsR8D9GqB6SS79Nd_5eFqpXp4V9G5GD-Zmhth22TBfsnZU5nUVGFyf/pub?gid=672067850&single=true&output=csv",
+  LINKS_CSV_URL:  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNae-sHgBR0y3X1VUQlOwl-gQsR8D9GqB6SS79Nd_5eFqpXp4V9G5GD-Zmhth22TBfsnZU5nUVGFyf/pub?gid=367798854&single=true&output=csv",
 
   // ---- Status write-back (Mark paid / Mark cleared) ----
   STATUS_WRITE_URL: "https://script.google.com/macros/s/AKfycbxOavoP_ng6ThR1ZI_fdSLynkEE25qtWN9qQX1qY4W0W9fZYhFRcvj_waq-eJ4QbEMh/exec",
